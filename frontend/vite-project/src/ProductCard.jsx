9971a91ab9750620d522d800cf4aad4305baf72b
caleblyk12 <caleblyk12@gmail.com>
2025-08-29 22:58:48 +0800

    using prop urls instead of dictionary in frontend

--- frontend/vite-project/src/ProductCard.jsx
+++ frontend/vite-project/src/ProductCard.jsx
@@ -1,12 +1,5 @@
 import {useState} from 'react'
 
-const productImages = {
-  1: "https://example.com/tiktok-hoodie.jpg",
-  2: "https://example.com/tiktok-cap.jpg",
-  5: "https://example.com/medium-water-bottle.jpg",
-  6: "https://example.com/small-water-bottle.jpg",
-  7: "https://example.com/large-water-bottle.jpg",
-};
 
 function ProductCard({product}){
 
@@ -14,7 +7,7 @@ function ProductCard({product}){
         <>
             <div className="product-card">
                 <h4>{product.name}</h4>
-                <img src={productImages[product.id]} alt={product.name}></img>
+                <img src={product.url} alt={product.name}></img>
                 <p>{product.price}</p>
             </div>
         </>
