75f1446575ba892cb5e379a449f8db2982d9fc09
caleblyk12 <caleblyk12@gmail.com>
2025-08-29 22:44:40 +0800

    urls for product images

--- backend/main.py
+++ backend/main.py
@@ -25,13 +25,13 @@ app.add_middleware(
 # Hardcoded Product Data
 # ------------------------------
 PRODUCTS = [
-    {"id": 1, "name": "Cool Hoodie", "price": "$29.99", "shipping": "3-5 days", "colours": "blue, red, green", "description": "made of cotton"},
-    {"id": 2, "name": "TikTok Hoodie", "price": "$39.99", "shipping": "7 days", "colours": "yellow", "description": "made of polyester"},
-    {"id": 3, "name": "Muscle T Shirt", "price": "$49.99", "shipping": "6 days", "colours": "black, white", "description": "drifit, compression tee"},
-    {"id": 4, "name": "TikTok Cap", "price": "$19.99", "shipping": "2-3 days", "colours": "black, white", "description": "moisture wicking, 5 panel"},
-    {"id": 5, "name": "Medium Water Bottle", "price": "$14.99", "shipping": "5-7 days", "colours": "pink, green", "description": "thermal insulating, 500ml"},
-    {"id": 6, "name": "Small Water Bottle", "price": "$10.99", "shipping": "2 days", "colours": "blue", "description": "thermal insulating, 250ml"},
-    {"id": 7, "name": "Large Water Bottle", "price": "$17.99", "shipping": "10 days", "colours": "blue, red", "description": "thermal insulating, 750ml"}
+    {"id": 1, "name": "Cool Hoodie", "price": "$29.99", "shipping": "3-5 days", "colours": "blue, red, green", "description": "made of cotton", "url": "https://images-na.ssl-images-amazon.com/images/I/61q6KBg7LWL._AC_UL1339_.jpg"},
+    {"id": 2, "name": "TikTok Hoodie", "price": "$39.99", "shipping": "7 days", "colours": "black", "description": "made of polyester", "url": "https://i.etsystatic.com/28439783/r/il/be5c0c/2996437775/il_1588xN.2996437775_ds1r.jpg"},
+    {"id": 3, "name": "Muscle T Shirt", "price": "$49.99", "shipping": "6 days", "colours": "black, white", "description": "drifit, compression tee", "url": "https://i.pinimg.com/originals/e9/f8/0d/e9f80dbe39759cf38646450dd610a902.jpg"},
+    {"id": 4, "name": "TikTok Cap", "price": "$19.99", "shipping": "2-3 days", "colours": "black, white", "description": "moisture wicking, 5 panel", "url": "https://img.joomcdn.net/9209c37d5f90bdcd70544a51d2093e37e4ccd048_original.jpeg"},
+    {"id": 5, "name": "Medium Water Bottle", "price": "$14.99", "shipping": "5-7 days", "colours": "pink, green", "description": "thermal insulating, 500ml", "url": "https://img.freepik.com/premium-photo/pink-thermos-bottle-isolated-white-background_884296-42265.jpg"},
+    {"id": 6, "name": "Small Water Bottle", "price": "$10.99", "shipping": "2 days", "colours": "blue", "description": "thermal insulating, 250ml", "url": "https://market.promarket-eu.com/wp-content/uploads/2020/12/Termo-AQUAPHOR.jpg"},
+    {"id": 7, "name": "Large Water Bottle", "price": "$17.99", "shipping": "10 days", "colours": "blue, red", "description": "thermal insulating, 750ml", "url": "https://assets.fishersci.com/TFS-Assets/CCG/Thermo-Scientific/product-images/19-1417229-0340916D-STD-01.jpg-650.jpg"}
 ]
 
 # ------------------------------
